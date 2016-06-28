46262d6510068d861d0b5a8237a7a0a8a35b8e4d
sbalev <stefan.balev@gmail.com>
2016-06-28 18:35:48 +0200

    Displaying the text chunk over the group

--- visu/svg/group_views_range.js
+++ visu/svg/group_views_range.js
@@ -6,6 +6,7 @@ const groups = require('../../modeles/data/groups.json').groups;
 const cabins = require('../../modeles/data/all_cabins.json').cabins;
 const stripes  = require('../../modeles/decret2/colors.json').colors;
 const colors = require('../src/colors.json').colors;
+const chunks = require('../../modeles/decret2/chunks.json').chunks;
 
 const cWidth = 44;
 const pWidth = cWidth / 2;
@@ -16,10 +17,22 @@ const yGap = 36;
 
 const widths = [2, 3, 5, 7, 9, 11];
 
-function writeGroup(out, y, stripes, label) {
+function writeGroup(out, y, stripes, label, chunks) {
     out.write('  <g>\n');
-    out.write(`    <text x= "0" y = "${y - lGap}" font-family="sans-serif" font-size="15">${label}</text>\n`);
+    // out.write(`    <text x="0" y="${y - lGap}" font-family="sans-serif" font-size="15">${label}</text>\n`);
+    out.write(`    <rect x="${-2 * cWidth}" y="${y}" width="${2 * cWidth}" height="${cHeight}" fill="none" stroke="none" />\n`)
+    out.write(`    <text x="${-lGap}" y="${y + (cHeight-15) / 2 + 15}" text-anchor="end" font-family="sans-serif" font-size="15">${label}</text>\n`);
     let x = 0;
+    if (chunks) {
+        for (let c = 0; c < chunks.length; c++) {
+            let dx = chunks[c].count * (cWidth + gap);
+            out.write(`    <path d="M ${x} ${y - lGap - 15} v 15" stroke="#808080" stroke-width="0.5"/>\n`);
+            out.write(`    <path d="M ${x + dx - gap} ${y - lGap - 15} v 15" stroke="#808080" stroke-width="0.5"/>\n`);
+            out.write(`    <text x="${x + dx / 2}" y="${y - lGap}" text-anchor="middle" font-family="sans-serif" font-size="15">${chunks[c].text}</text>\n`);
+            x += dx;
+        }
+    }
+    x = 0;
     for (let i = 0; i < stripes.length; i++) {
         let w = Math.floor(stripes[i] / 10);
         let c = stripes[i] % 10;
@@ -52,7 +65,7 @@ function writeGroups(gStart, gEnd, fileName) {
             let s = stripes[cabins[start + i].id].stripes;
             gs.push(s[0], s[1]);
         }
-        writeGroup(out, y, gs, `${groups[g].id} front`);
+        writeGroup(out, y, gs, `${groups[g].id} (F)`);
         y += cHeight + yGap;
     }
 
@@ -63,11 +76,16 @@ function writeGroups(gStart, gEnd, fileName) {
             let s = stripes[cabins[start + i].id].stripes;
             gs.push(s[4], s[5]);
         }
-        writeGroup(out, y, gs, `${groups[g].id} rear`);
+        writeGroup(out, y, gs, `${groups[g].id} (R)`, chunks[groups[g].id]);
         y += cHeight + yGap;
     }
     out.write('</svg>\n');
     out.end();
 }
 
-writeGroups(32, 38, 'test.svg');
+// writeGroups(0, 8, 'p1.svg');
+writeGroups(8, 14, 'p2.svg');
+writeGroups(14, 20, 'p3.svg');
+writeGroups(20, 26, 'p4.svg');
+writeGroups(26, 32, 'p5.svg');
+writeGroups(32, 38, 'p6.svg');
