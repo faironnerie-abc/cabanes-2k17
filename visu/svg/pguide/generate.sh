8478bbabe471024772c71c32907cf859cd082a7e
sbalev <stefan.balev@gmail.com>
2016-11-09 14:26:49 +0100

    Licence + hack to export hyperlinks in pdf

--- visu/svg/pguide/generate.sh
+++ visu/svg/pguide/generate.sh
@@ -6,5 +6,10 @@ for f in `ls a*.svg b*.svg p*.svg`; do
     inkscape -T -A $out $f
     echo $out
 done
+
+# hack for the hyperlinks p. ii
+svglinkify.py b02.svg b02.pdf b02_links.pdf
+mv b02_links.pdf b02.pdf
+
 pdftk a*.pdf b*.pdf p*.pdf cat output PaintDoc.pdf
 rm a*.pdf b*.pdf p*.pdf p*.svg
